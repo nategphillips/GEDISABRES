71719668318e3297237db5d0d361d06d29ffd8d9
AMilkyBoi <30729007+AMilkyBoi@users.noreply.github.com>
2023-08-24 12:02:05 -0500

    easier plotting

--- src/input.py
+++ src/input.py
@@ -15,7 +15,7 @@ PRES: float = 101325.0
 ROT_LVLS: np.ndarray = np.arange(0, 37, 1)
 
 # List of vibrational transitions considered in (v', v'') format
-VIB_BANDS: list[tuple] = [(2, 0), (1, 0), (0, 0)]
+VIB_BANDS: list[tuple] = [(2, 0), (1, 0)]
 
 # Line data
 LINE_DATA: bool = True
@@ -24,7 +24,10 @@ LINE_DATA: bool = True
 CONV_DATA: bool = True
 
 # Sample data
-SAMP_DATA: bool = False
+SAMP_DATA: bool = True
+SAMP_FILE: list[str] = ['harvard', 'pgopher']
+SAMP_COLS: list[str] = ['purple', 'skyblue']
+SAMP_LABL: list[str] = ['Harvard Data', 'PGOPHER Data']
 
 # General plotting
 PLOT_SAVE:  bool  = False

5183c4379d441617beb9cd0464239847115162bc
AMilkyBoi <30729007+AMilkyBoi@users.noreply.github.com>
2023-10-02 13:52:10 -0500

    improve lif simulation, add conv to lif

--- src/initialize.py
+++ src/initialize.py
@@ -3,6 +3,8 @@
 Initializes each spectral line by applying valid selection rules.
 '''
 
+import itertools
+
 import numpy as np
 
 from energy import State
@@ -24,40 +26,39 @@ def selection_rules(rot_qn_list: np.ndarray, pd_data) -> np.ndarray:
     # Empty list to contain all valid spectral lines
     lines = []
 
-    for grnd_rot_qn in rot_qn_list:
-        for exct_rot_qn in rot_qn_list:
-            # Remove every even R and P line since the nuclear spin of oxygen is zero
-            if grnd_rot_qn % 2 == 1:
+    for gnd_rot_qn, ext_rot_qn in itertools.product(rot_qn_list, repeat=2):
+        d_rot_qn = ext_rot_qn - gnd_rot_qn
 
-                # Selection rules for the R branch
-                if exct_rot_qn - grnd_rot_qn == 1:
-                    for grnd_branch_idx in range(1, 4):
-                        for exct_branch_idx in range(1, 4):
-                            if grnd_branch_idx == exct_branch_idx:
-                                lines.append(SpectralLine(grnd_rot_qn, exct_rot_qn, 'r',
-                                                          grnd_branch_idx, exct_branch_idx, 0.0))
-                            if grnd_branch_idx > exct_branch_idx:
-                                lines.append(SpectralLine(grnd_rot_qn, exct_rot_qn, 'rq',
-                                                          grnd_branch_idx, exct_branch_idx, 0.0))
+        # For molecular oxygen, all transitions with even values of J'' are forbidden
+        if gnd_rot_qn % 2 == 1:
 
-                # Selection rules for the P branch
-                elif exct_rot_qn - grnd_rot_qn == -1:
-                    for grnd_branch_idx in range(1, 4):
-                        for exct_branch_idx in range(1, 4):
-                            if grnd_branch_idx == exct_branch_idx:
-                                lines.append(SpectralLine(grnd_rot_qn, exct_rot_qn, 'p',
-                                                          grnd_branch_idx, exct_branch_idx, 0.0))
-                            if grnd_branch_idx < exct_branch_idx:
-                                lines.append(SpectralLine(grnd_rot_qn, exct_rot_qn, 'pq',
-                                                          grnd_branch_idx, exct_branch_idx, 0.0))
+            # Selection rules for the R branch
+            if d_rot_qn == 1:
+                for gnd_branch_idx, ext_branch_idx in itertools.product(range(1, 4), repeat=2):
+                    if gnd_branch_idx == ext_branch_idx:
+                        lines.append(SpectralLine(gnd_rot_qn, ext_rot_qn, 'r',
+                                                    gnd_branch_idx, ext_branch_idx, 0.0))
+                    if gnd_branch_idx > ext_branch_idx:
+                        lines.append(SpectralLine(gnd_rot_qn, ext_rot_qn, 'rq',
+                                                    gnd_branch_idx, ext_branch_idx, 0.0))
+
+            # Selection rules for the P branch
+            elif d_rot_qn == -1:
+                for gnd_branch_idx, ext_branch_idx in itertools.product(range(1, 4), repeat=2):
+                    if gnd_branch_idx == ext_branch_idx:
+                        lines.append(SpectralLine(gnd_rot_qn, ext_rot_qn, 'p',
+                                                    gnd_branch_idx, ext_branch_idx, 0.0))
+                    if gnd_branch_idx < ext_branch_idx:
+                        lines.append(SpectralLine(gnd_rot_qn, ext_rot_qn, 'pq',
+                                                    gnd_branch_idx, ext_branch_idx, 0.0))
 
     for line in lines:
-        if line.grnd_branch_idx == 1:
-            line.predissociation = pd_data['f1'][pd_data['rot_qn'] == line.exct_rot_qn].iloc[0]
-        elif line.grnd_branch_idx == 2:
-            line.predissociation = pd_data['f2'][pd_data['rot_qn'] == line.exct_rot_qn].iloc[0]
+        if line.gnd_branch_idx == 1:
+            line.predissociation = pd_data['f1'][pd_data['rot_qn'] == line.ext_rot_qn].iloc[0]
+        elif line.gnd_branch_idx == 2:
+            line.predissociation = pd_data['f2'][pd_data['rot_qn'] == line.ext_rot_qn].iloc[0]
         else:
-            line.predissociation = pd_data['f3'][pd_data['rot_qn'] == line.exct_rot_qn].iloc[0]
+            line.predissociation = pd_data['f3'][pd_data['rot_qn'] == line.ext_rot_qn].iloc[0]
 
     return np.array(lines)
 
@@ -66,13 +67,13 @@ class SpectralLine:
     Holds the necessary data for a single spectral line.
     '''
 
-    def __init__(self, grnd_rot_qn: int, exct_rot_qn: int, branch: str, grnd_branch_idx: int,
-                 exct_branch_idx: int, predissociation: float) -> None:
-        self.grnd_rot_qn     = grnd_rot_qn
-        self.exct_rot_qn     = exct_rot_qn
+    def __init__(self, gnd_rot_qn: int, ext_rot_qn: int, branch: str, gnd_branch_idx: int,
+                 ext_branch_idx: int, predissociation: float) -> None:
+        self.gnd_rot_qn      = gnd_rot_qn
+        self.ext_rot_qn      = ext_rot_qn
         self.branch          = branch
-        self.grnd_branch_idx = grnd_branch_idx
-        self.exct_branch_idx = exct_branch_idx
+        self.gnd_branch_idx  = gnd_branch_idx
+        self.ext_branch_idx  = ext_branch_idx
         self.predissociation = predissociation
 
     def wavenumber(self, band_origin: float, grnd_state: 'State', exct_state: 'State') -> float:
@@ -90,8 +91,8 @@ class SpectralLine:
         '''
 
         return band_origin + \
-               energy.rotational_term(self.exct_rot_qn, exct_state, self.exct_branch_idx) - \
-               energy.rotational_term(self.grnd_rot_qn, grnd_state, self.grnd_branch_idx)
+               energy.rotational_term(self.ext_rot_qn, exct_state, self.ext_branch_idx) - \
+               energy.rotational_term(self.gnd_rot_qn, grnd_state, self.gnd_branch_idx)
 
     def intensity(self, band_origin: float, grnd_state: 'State', exct_state: 'State',
                   temp: float) -> float:
@@ -114,19 +115,19 @@ class SpectralLine:
 
         # The basic intensity function if no branches are considered
         base = (self.wavenumber(band_origin, grnd_state, exct_state) / part) * \
-               np.exp(- (energy.rotational_term(self.grnd_rot_qn, grnd_state, \
-               self.grnd_branch_idx) * cn.PLANC * cn.LIGHT) / (cn.BOLTZ * temp))
+               np.exp(- (energy.rotational_term(self.gnd_rot_qn, grnd_state, \
+               self.gnd_branch_idx) * cn.PLANC * cn.LIGHT) / (cn.BOLTZ * temp))
 
         # Intensity is dependent upon branch, with satellite branches having a much lower intensity
         # (notice that r and p scale with N**2, while rq and rp scale with 1/N**2)
         if self.branch == 'r':
-            linestr = ((self.grnd_rot_qn + 1)**2 - 0.25) / (self.grnd_rot_qn + 1)
+            linestr = ((self.gnd_rot_qn + 1)**2 - 0.25) / (self.gnd_rot_qn + 1)
             intn =  base * linestr
         elif self.branch == 'p':
-            linestr  = ((self.grnd_rot_qn)**2 - 0.25) / (self.grnd_rot_qn)
+            linestr  = ((self.gnd_rot_qn)**2 - 0.25) / (self.gnd_rot_qn)
             intn =  base * linestr
         else:
-            linestr = (2 * self.grnd_rot_qn + 1) / (4 * self.grnd_rot_qn * (self.exct_rot_qn + 1))
+            linestr = (2 * self.gnd_rot_qn + 1) / (4 * self.gnd_rot_qn * (self.ext_rot_qn + 1))
             intn = base * linestr
 
         # Naive approach of applying 1:2:1 line intensity ratio to each band, this way the two peaks
@@ -134,7 +135,7 @@ class SpectralLine:
 
         # NOTE: this *seems* to be what PGOPHER is doing from what I can tell, also haven't been
         #       able to find anything in Herzberg about it yet
-        if self.grnd_branch_idx in (1, 3):
+        if self.gnd_branch_idx in (1, 3):
             return intn / 2
 
         return intn

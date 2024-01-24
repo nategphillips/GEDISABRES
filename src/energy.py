3488f12a90a2a064a67d4474948427bfd2701f51
AMilkyBoi <30729007+AMilkyBoi@users.noreply.github.com>
2024-01-24 13:12:15 -0600

    double checked spin constants, working on a triplet line fix

--- src/energy.py
+++ src/energy.py
@@ -89,9 +89,6 @@ def rotational_term(rot_qn: int, state: 'State', branch_idx: int) -> float:
         float: rotational term F(N)
     '''
 
-    first_term = state.rotational_constants()[0] * rot_qn * (rot_qn + 1) - \
-                 state.rotational_constants()[1] * rot_qn**2 * (rot_qn + 1)**2 + \
-                 state.rotational_constants()[2] * rot_qn**3 * (rot_qn + 1)**3
 
     # TODO: 9/24/23 this was causing the weird lines appearing where they shouldn't be, removing it
     #               fixed the issue of random lines at high wavenumbers. Matches data much better &
@@ -104,26 +101,58 @@ def rotational_term(rot_qn: int, state: 'State', branch_idx: int) -> float:
     # else:
     #     sqrt_sign = 1
 
-    sqrt_sign = 1
+    # sqrt_sign = 1
 
-    # FIXME: 9/19/23 the sign in front of the state.spn_const[0] should 100% be a negative. I've
-    #                checked multiple sources at this point. I need to find out why there seems to
-    #                be issues with how the triplet branches are spaced. leaving it positive for now
-    #                since it seems to give better results
+    # first_term = state.rotational_constants()[0] * rot_qn * (rot_qn + 1) - \
+    #              state.rotational_constants()[1] * rot_qn**2 * (rot_qn + 1)**2 + \
+    #              state.rotational_constants()[2] * rot_qn**3 * (rot_qn + 1)**3
+
+    # # FIXME: 9/19/23 the sign in front of the state.spn_const[0] should 100% be a negative. I've
+    # #                checked multiple sources at this point. I need to find out why there seems to
+    # #                be issues with how the triplet branches are spaced. leaving it positive for now
+    # #                since it seems to give better results
+
+    # match branch_idx:
+    #     case 1:
+    #         return first_term + (2 * rot_qn + 3) * state.rotational_constants()[0] - \
+    #                state.spn_consts[0] - sqrt_sign * np.sqrt((2 * rot_qn + 3)**2 * \
+    #                state.rotational_constants()[0]**2 + state.spn_consts[0]**2 - 2 * \
+    #                state.spn_consts[0] * state.rotational_constants()[0]) + \
+    #                state.spn_consts[1] * (rot_qn + 1)
+
+    #     case 3:
+    #         return first_term - (2 * rot_qn - 1) * state.rotational_constants()[0] - \
+    #                state.spn_consts[0] + sqrt_sign * np.sqrt((2 * rot_qn - 1)**2 * \
+    #                state.rotational_constants()[0]**2 + state.spn_consts[0]**2 - 2 * \
+    #                state.spn_consts[0] * state.rotational_constants()[0]) - \
+    #                state.spn_consts[1] * rot_qn
+
+    #     case _:
+    #         return first_term
+
+    # NOTE: 01/24/24 this formulation (from Bergeman) of the triplets uses the negative
+    #                spin-rotation constant in the upper term and works well. I still need to figure
+    #                out the Hamiltonian matrix elements given since they give a second term for the
+    #                two triplets (and I'm not sure how to implement it correctly)
+
+    # FIXME: 01/24/24 work in progress (see note)
+
+    x = rot_qn * (rot_qn + 1)
+    b = state.rotational_constants()[0]
+    d = state.rotational_constants()[1]
+    h = state.rotational_constants()[2]
+    l = state.spn_consts[0]
+    g = state.spn_consts[1]
+
+    e0 = x * b - x**2 * d + x**3 * h
+    e1 = x * b - (x**2 + 4 * x) * d
+    e2 = (x + 2) * b - (x**2 + 8 * x + 4) * d - 2 * l - g
+    thing = - 2 * np.sqrt(x) * (b - 2 * (x + 1) * d - g / 2)
+
     match branch_idx:
         case 1:
-            return first_term + (2 * rot_qn + 3) * state.rotational_constants()[0] + \
-                   state.spn_consts[0] - sqrt_sign * np.sqrt((2 * rot_qn + 3)**2 * \
-                   state.rotational_constants()[0]**2 + state.spn_consts[0]**2 - 2 * \
-                   state.spn_consts[0] * state.rotational_constants()[0]) + \
-                   state.spn_consts[1] * (rot_qn + 1)
-
+            return e1
         case 3:
-            return first_term - (2 * rot_qn - 1) * state.rotational_constants()[0] - \
-                   state.spn_consts[0] + sqrt_sign * np.sqrt((2 * rot_qn - 1)**2 * \
-                   state.rotational_constants()[0]**2 + state.spn_consts[0]**2 - 2 * \
-                   state.spn_consts[0] * state.rotational_constants()[0]) - \
-                   state.spn_consts[1] * rot_qn
-
+            return e2
         case _:
-            return first_term
+            return e0

073c4c4fddf56766bb8437de4debac40c0c76d06
AMilkyBoi <30729007+AMilkyBoi@users.noreply.github.com>
2024-01-26 14:54:21 -0600

    restructure mass and radius calculations

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,7 @@ Physical and diatomic constants for the X3Σg- to B3Σu- transition of oxygen.
 '''
 
 import pandas as pd
+import numpy as np
 
 import input as inp
 
@@ -20,3 +21,19 @@ MOLECULAR_CONSTS = pd.read_csv(f'../data/molecular_constants/{inp.MOLECULE}.csv'
 # functionality of the pandas series isn't needed
 CONSTS_UP = MOLECULAR_CONSTS.loc[inp.STATE_UP].to_dict()
 CONSTS_LO = MOLECULAR_CONSTS.loc[inp.STATE_LO].to_dict()
+
+# atomic masses for diatoms [amu]
+MASS_DATA = pd.read_csv('../data/molecular_masses.csv', index_col=0).loc[inp.MOLECULE].to_dict()
+
+# mass of atoms [kg]
+MASS_ATOM_1 = MASS_DATA['atom_1'] / AVOGD / 1e3
+MASS_ATOM_2 = MASS_DATA['atom_2'] / AVOGD / 1e3
+
+# molecular mass [kg]
+MASS_MOLECULE = MASS_ATOM_1 + MASS_ATOM_2
+
+# reduced mass [kg]
+MASS_REDUCED = (MASS_ATOM_1 * MASS_ATOM_2) / MASS_MOLECULE
+
+# ground state cross-sectional area
+CROSS_SEC = np.pi * (CONSTS_LO['rad'] + CONSTS_LO['rad'])**2

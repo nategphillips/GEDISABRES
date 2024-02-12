5e70072a734efaf30472f0a36a700743b33e4d47
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-02-11 18:05:39 -0600

    more updates

--- src/test.py
+++ src/test.py
@@ -29,11 +29,11 @@ class Simulation:
         self.rot_levels    = rot_levels
         self.upper_state   = ElectronicState(upper_state, self.molecule.consts)
         self.lower_state   = ElectronicState(lower_state, self.molecule.consts)
-        self.allowed_lines = self.selection_rules()
+        self.allowed_lines = self.get_allowed_lines()
         self.vib_bands     = [VibrationalBand(vib_band, self.allowed_lines,
                              self.upper_state, self.lower_state) for vib_band in vib_bands]
 
-    def selection_rules(self):
+    def get_allowed_lines(self):
         lines = []
 
         for gnd_rot_qn, ext_rot_qn in itertools.product(self.rot_levels, repeat=2):
@@ -75,26 +75,8 @@ class ElectronicState:
         self.name   = name
         self.consts = consts.loc[self.name].to_dict()
 
-    def rotational_constants(self, vib_qn):
-        b_v = self.consts['b_e']                        - \
-              self.consts['alph_e'] * (vib_qn + 0.5)    + \
-              self.consts['gamm_e'] * (vib_qn + 0.5)**2 + \
-              self.consts['delt_e'] * (vib_qn + 0.5)**3
-
-        d_v = self.consts['d_e'] - self.consts['beta_e'] * (vib_qn + 0.5)
-
-        h_v = self.consts['h_e']
-
-        return [b_v, d_v, h_v]
-
     def electronic_term(self):
         return self.consts['t_e']
-
-    def vibrational_term(self, vib_qn):
-        return self.consts['w_e']   * (vib_qn + 0.5)    - \
-               self.consts['we_xe'] * (vib_qn + 0.5)**2 + \
-               self.consts['we_ye'] * (vib_qn + 0.5)**3 + \
-               self.consts['we_ze'] * (vib_qn + 0.5)**4
 
 class VibrationalBand:
     def __init__(self, name, lines, upper_state, lower_state):
@@ -104,17 +86,36 @@ class VibrationalBand:
         self.upper_state = upper_state
         self.lower_state = lower_state
 
+    def vibrational_term(self, state, vib_qn):
+        return state.consts['w_e']   * (vib_qn + 0.5)    - \
+               state.consts['we_xe'] * (vib_qn + 0.5)**2 + \
+               state.consts['we_ye'] * (vib_qn + 0.5)**3 + \
+               state.consts['we_ze'] * (vib_qn + 0.5)**4
+
+    def rotational_constants(self, state, vib_qn):
+        b_v = state.consts['b_e']                        - \
+              state.consts['alph_e'] * (vib_qn + 0.5)    + \
+              state.consts['gamm_e'] * (vib_qn + 0.5)**2 + \
+              state.consts['delt_e'] * (vib_qn + 0.5)**3
+
+        d_v = state.consts['d_e'] - state.consts['beta_e'] * (vib_qn + 0.5)
+
+        h_v = state.consts['h_e']
+
+        return [b_v, d_v, h_v]
+
     def band_origin(self):
+        # called once per band
         elc_energy = self.upper_state.electronic_term() - \
                      self.lower_state.electronic_term()
 
-        vib_energy = self.upper_state.vibrational_term(self.vib_qn_up) - \
-                     self.lower_state.vibrational_term(self.vib_qn_lo)
+        vib_energy = self.vibrational_term(self.upper_state, self.vib_qn_up) - \
+                     self.vibrational_term(self.lower_state, self.vib_qn_lo)
 
         return elc_energy + vib_energy
 
     def wavenumbers(self):
-        return np.array([line.wavenumber(self.band_origin()) for line in self.lines])
+        return np.array([line.wavenumber(self.band_origin(), self.upper_state, self.lower_state, self.vib_qn_up, self.vib_qn_lo) for line in self.lines])
 
     def intensities(self):
         return np.array([line.intensity() for line in self.lines])
@@ -127,11 +128,37 @@ class SpectralLine:
     triplet_idx_lo: int
     branch:         str
 
-    def rotational_term(self):
-        return 1
+    def rotational_term(self, rot_qn, vib_qn, state, branch_idx):
+        # called for each line
+        x = rot_qn * (rot_qn + 1)
+        b = state.rotational_constants(vib_qn)[0]
+        d = state.rotational_constants(vib_qn)[1]
+        h = state.rotational_constants(vib_qn)[2]
 
-    def wavenumber(self, band_origin):
-        return band_origin
+        # accessed
+        l = state.consts['lamd']
+        g = state.consts['gamm']
+
+        single = x * b - x**2 * d + x**3 * h
+
+        e0 = x * b - (x**2 + 4 * x) * d
+        e1 = (x + 2) * b - (x**2 + 8 * x + 4) * d - 2 * l - g
+        odiag = - 2 * np.sqrt(x) * (b - 2 * (x + 1) * d - g / 2)
+
+        energy1 = 0.5 * ((e0 + e1) + np.sqrt((e0 + e1)**2 - 4 * (e0 * e1 - odiag**2)))
+        energy2 = 0.5 * ((e0 + e1) - np.sqrt((e0 + e1)**2 - 4 * (e0 * e1 - odiag**2)))
+
+        match branch_idx:
+            case 1:
+                return energy1
+            case 3:
+                return energy2
+            case _:
+                return single
+
+    def wavenumber(self, band_origin, state_up, state_lo, vib_qn_up, vib_qn_lo):
+        return band_origin + self.rotational_term(self.rot_qn_up, vib_qn_up, state_up, self.triplet_idx_up) - \
+                             self.rotational_term(self.rot_qn_lo, vib_qn_lo, state_lo, self.triplet_idx_lo)
 
     def intensity(self):
         return 1

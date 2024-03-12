58f44f7ee1a1de41f5ccf715b9c4b2f18cea5dd9
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-03-11 22:22:45 -0500

    only compute certain broadening params once

--- src/convolve.py
+++ src/convolve.py
@@ -23,9 +23,11 @@ def convolve_inst(wavenumbers_conv: np.ndarray, intensities_conv: np.ndarray,
 def convolve_brod(sim: Simulation, lines: np.ndarray, wavenumbers_line: np.ndarray,
                   intensities_line: np.ndarray, wavenumbers_conv: np.ndarray) -> np.ndarray:
     intensities_conv = np.zeros_like(wavenumbers_conv)
+    natural, collide = broadening_params(sim)
 
     for idx, (wave, intn) in enumerate(zip(wavenumbers_line, intensities_line)):
-        intensities_conv += intn * broadening_fn(sim, lines, wavenumbers_conv, wave, idx)
+        intensities_conv += intn * broadening_fn(sim, lines, wavenumbers_conv, wave, idx, natural,
+                                                 collide)
 
     return intensities_conv
 
@@ -35,28 +37,26 @@ def instrument_fn(convolved_wavenumbers: np.ndarray, wavenumber_peak: float,
            (broadening * np.sqrt(2 * np.pi))
 
 def broadening_fn(sim: Simulation, lines: np.ndarray, convolved_wavenumbers: np.ndarray,
-                  wavenumber_peak: float, line_idx: int) -> np.ndarray:
+                  wavenumber_peak: float, line_idx: int, natural: float,
+                  collide: float) -> np.ndarray:
 
-    # natural (Lorentzian)
-    natural = sim.state_lo.cross_section**2 * \
-              np.sqrt(8 / (np.pi * sim.molecule.reduced_mass * cn.BOLTZ * sim.temp)) / 4
-
-    # doppler (Gaussian)
     doppler = wavenumber_peak * \
               np.sqrt(cn.BOLTZ * sim.temp / (sim.molecule.molecular_mass * (cn.LIGHT / 1e2)**2))
 
-    # collisional (Lorentzian)
-    # convert pressure in [N/m^2] to pressure in [dyne/cm^2]
-    collide = (sim.pres * 10) * sim.state_lo.cross_section**2 * \
-              np.sqrt(8 / (np.pi * sim.molecule.reduced_mass * cn.BOLTZ * sim.temp)) / 2
-
-    # predissociation (Lorentzian)
     prediss = lines[line_idx].predissociation()
 
     gauss = doppler
     loren = natural + collide + prediss
 
-    # Faddeeva function
     fadd = ((convolved_wavenumbers - wavenumber_peak) + 1j * loren) / (gauss * np.sqrt(2))
 
     return np.real(wofz(fadd)) / (gauss * np.sqrt(2 * np.pi))
+
+def broadening_params(sim: Simulation):
+    natural = sim.state_lo.cross_section**2 * \
+              np.sqrt(8 / (np.pi * sim.molecule.reduced_mass * cn.BOLTZ * sim.temp)) / 4
+
+    collide = (sim.pres * 10) * sim.state_lo.cross_section**2 * \
+              np.sqrt(8 / (np.pi * sim.molecule.reduced_mass * cn.BOLTZ * sim.temp)) / 2
+
+    return natural, collide

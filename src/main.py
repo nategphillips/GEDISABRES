1d4dc47b8c28b7a7662704df9730a7471bf45ee4
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-06-04 11:38:55 -0500

    add correctly calculated lif spectra

--- src/main.py
+++ src/main.py
@@ -21,17 +21,13 @@ def main():
     mol_o2 = Molecule('o2', 'o', 'o')
 
     bands_sim = [(2, 0)]
-    bands_lif = [(7, v) for v in range(1, -1, -1)]
+    bands_lif = [(7, v) for v in range(12, -1, -1)]
 
     o2_sim = Simulation(mol_o2, temp, pres, np.arange(0, 36), 'b3su', 'x3sg', bands_sim,
                         SimType.ABSORPTION)
 
-    # FIXME: 05/06/24 - LIF currently not working since all lines are simulated in each band in
-    #        order for the the rotational partition function to work properly; need to only plot the
-    #        selected lines with the correct quantum numbers
-
-    # o2_lif = Simulation(mol_o2, temp, pres, np.arange(0, 36), 'b3su', 'x3sg', bands_lif, rot_qn_up,
-    #                     rot_qn_lo)
+    o2_lif = Simulation(mol_o2, temp, pres, np.arange(0, 36), 'b3su', 'x3sg', bands_lif,
+                        SimType.LIF)
 
     palette_lif = plt.cycler('color', plt.cm.tab20c.colors).by_key()['color']
     colors_lif  = [matplotlib.colors.to_hex(color) for color in palette_lif]
@@ -41,22 +37,25 @@ def main():
     #        bands are iterated through, meaning the wavelength and intensity info for each band is
     #        potentially being re-calculated several times
 
-    # plot.plot_line(o2_lif, colors_lif)
-    # plot.plot_info(o2_lif)
-    # plot.plot_show()
+    # TODO: 06/04/24 - remove SimType and just have the option to plot absorption or emission
+    #       through the plots
 
-    # plot.plot_line(o2_sim, colors_lif)
-    plot.plot_conv(o2_sim, colors_lif)
-    # plot.plot_samp('pgopher', 'red', 'stem')
-    plot.plot_samp('harvard/harvard20', 'red', 'plot')
-    plot.plot_residual(o2_sim, 'green', 'harvard/harvard20')
-
-    # testing how the PGOPHER data compares when convolved (estimating predissociation rates)
-    from test import cwls, cins
-
-    plt.plot(cwls, cins, 'black', label='pgopher')
-
+    plot.plot_lif(o2_lif, rot_qn_up, rot_qn_lo, colors_lif)
+    plot.plot_lif_info(o2_lif, rot_qn_up, rot_qn_lo)
     plot.plot_show()
+
+    # # plot.plot_line(o2_sim, colors_lif)
+    # plot.plot_conv(o2_sim, colors_lif)
+    # # plot.plot_samp('pgopher', 'red', 'stem')
+    # plot.plot_samp('harvard/harvard20', 'red', 'plot')
+    # plot.plot_residual(o2_sim, 'green', 'harvard/harvard20')
+
+    # # testing how the PGOPHER data compares when convolved (estimating predissociation rates)
+    # from test import cwls, cins
+
+    # plt.plot(cwls, cins, 'black', label='pgopher')
+
+    # plot.plot_show()
 
 if __name__ == '__main__':
     main()
--- src/plot.py
+++ src/plot.py
@@ -46,7 +46,7 @@ def plot_samp(samp_file: str, color: str, plot_as: str = 'stem') -> None:
         case _:
             raise ValueError(f'Invalid value for plot_as: {plot_as}.')
 
-def plot_info(sim: Simulation) -> None:
+def plot_line_info(sim: Simulation) -> None:
     for vib_band in sim.vib_bands:
         wavenumbers_line = vib_band.wavenumbers_line()
         wavelengths_line = wavenum_to_wavelen(wavenumbers_line)
@@ -61,6 +61,24 @@ def plot_line(sim: Simulation, colors: list) -> None:
         wavelengths_line = wavenum_to_wavelen(vib_band.wavenumbers_line())
 
         plt.stem(wavelengths_line, vib_band.intensities_line(), colors[idx], markerfmt='',
+                 label=f'{sim.molecule.name} {vib_band.vib_qn_up, vib_band.vib_qn_lo} line')
+
+def plot_lif_info(sim: Simulation, rot_qn_up: int, rot_qn_lo: int) -> None:
+    for vib_band in sim.vib_bands:
+        wavenumbers_line = vib_band.wavenumbers_lif(rot_qn_up, rot_qn_lo)
+        wavelengths_line = wavenum_to_wavelen(wavenumbers_line)
+        intensities_line = vib_band.intensities_lif(rot_qn_up, rot_qn_lo)
+        lines = vib_band.get_lif_lines(rot_qn_up, rot_qn_lo)
+
+        for idx, line in enumerate(lines):
+            plt.text(wavelengths_line[idx], intensities_line[idx], f'{line.branch_name}')
+
+def plot_lif(sim: Simulation, rot_qn_up: int, rot_qn_lo: int, colors: list) -> None:
+    for idx, vib_band in enumerate(sim.vib_bands):
+        wavelengths_lif = wavenum_to_wavelen(vib_band.wavenumbers_lif(rot_qn_up, rot_qn_lo))
+
+        plt.stem(wavelengths_lif, vib_band.intensities_lif(rot_qn_up, rot_qn_lo), colors[idx],
+                 markerfmt='',
                  label=f'{sim.molecule.name} {vib_band.vib_qn_up, vib_band.vib_qn_lo} line')
 
 def plot_conv(sim: Simulation, colors: list) -> None:

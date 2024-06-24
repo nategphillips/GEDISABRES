c9880f57985553309f81f459a854b929a9301dc8
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-06-24 11:05:48 -0500

    standardize syntax

--- src/molecule.py
+++ src/molecule.py
@@ -17,10 +17,10 @@ class Molecule:
         self.name:    str          = name
         self.atom_1:  Atom         = Atom(atom_1)
         self.atom_2:  Atom         = Atom(atom_2)
-        self.consts:  pd.DataFrame = pd.read_csv(f'../data/molecular_constants/{self.name}.csv',
+        self.consts:  pd.DataFrame = pd.read_csv(f"../data/molecular_constants/{self.name}.csv",
                                                  index_col=0)
-        self.prediss: pd.DataFrame = pd.read_csv(f'../data/predissociation/{self.name}.csv')
-        self.fc_data: np.ndarray   = np.loadtxt(f'../data/franck-condon/{self.name}.csv',
+        self.prediss: pd.DataFrame = pd.read_csv(f"../data/predissociation/{self.name}.csv")
+        self.fc_data: np.ndarray   = np.loadtxt(f"../data/franck-condon/{self.name}.csv",
                                                 delimiter=',')
         self.molecular_mass: float = self.atom_1.mass + self.atom_2.mass
         self.reduced_mass:   float = self.atom_1.mass * self.atom_2.mass / self.molecular_mass

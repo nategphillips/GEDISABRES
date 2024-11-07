935107251842a9fc87025478d92c68201337b345
amilkyboi <30729007+amilkyboi@users.noreply.github.com>
2024-11-07 14:52:49 -0600

    add option to plot sample files

--- src/plot.py
+++ src/plot.py
@@ -11,10 +11,23 @@ from sim import Sim
 import utils
 
 
+def plot_sample(axs: Axes, data: np.ndarray) -> None:
+    """
+    Plots sample data.
+    """
+
+    wavelengths: np.ndarray = utils.wavenum_to_wavelen(data[:, 0])
+    intensities: np.ndarray = data[:, 1]
+
+    axs.plot(wavelengths, intensities / intensities.max())
+
+
 def plot_line(axs: Axes, sim: Sim, colors: list[str]) -> None:
     """
     Plots each rotational line.
     """
+
+    max_intensity: float = sim.all_line_data()[1].max()
 
     for idx, band in enumerate(sim.bands):
         wavelengths_line: np.ndarray = utils.wavenum_to_wavelen(band.wavenumbers_line())
@@ -22,7 +35,7 @@ def plot_line(axs: Axes, sim: Sim, colors: list[str]) -> None:
 
         axs.stem(
             wavelengths_line,
-            intensities_line,
+            intensities_line / max_intensity,
             colors[idx],
             markerfmt="",
             label=f"{sim.molecule.name} {band.v_qn_up, band.v_qn_lo} line",
@@ -37,6 +50,8 @@ def plot_line_info(axs: Axes, sim: Sim, colors: list[str]) -> None:
     # In order to show text, a plot must first exist.
     plot_line(axs, sim, colors)
 
+    max_intensity: float = sim.all_line_data()[1].max()
+
     for band in sim.bands:
         # Only select non-satellite lines to reduce the amount of data on screen.
         lines: list[Line] = [line for line in band.lines if not line.is_satellite]
@@ -44,7 +59,7 @@ def plot_line_info(axs: Axes, sim: Sim, colors: list[str]) -> None:
         for line in lines:
             axs.text(
                 utils.wavenum_to_wavelen(line.wavenumber),
-                line.intensity,
+                line.intensity / max_intensity,
                 f"${line.branch_name}_{{{line.branch_idx_up}{line.branch_idx_lo}}}$",
             )
 
@@ -54,13 +69,15 @@ def plot_conv_sep(axs: Axes, sim: Sim, colors: list[str]) -> None:
     Plots convolved data for each vibrational band separately.
     """
 
+    max_intensity: float = sim.all_conv_data()[1].max()
+
     for idx, band in enumerate(sim.bands):
         wavelengths_conv: np.ndarray = utils.wavenum_to_wavelen(band.wavenumbers_conv())
         intensities_conv: np.ndarray = band.intensities_conv()
 
         axs.plot(
             wavelengths_conv,
-            intensities_conv,
+            intensities_conv / max_intensity,
             colors[idx],
             label=f"{sim.molecule.name} {band.v_qn_up, band.v_qn_lo} conv",
         )
@@ -74,4 +91,9 @@ def plot_conv_all(axs: Axes, sim: Sim, colors: list[str]) -> None:
     wavenumbers_conv, intensities_conv = sim.all_conv_data()
     wavelengths_conv: np.ndarray = utils.wavenum_to_wavelen(wavenumbers_conv)
 
-    axs.plot(wavelengths_conv, intensities_conv, colors[0], label=f"{sim.molecule.name} conv all")
+    axs.plot(
+        wavelengths_conv,
+        intensities_conv / intensities_conv.max(),
+        colors[0],
+        label=f"{sim.molecule.name} conv all",
+    )

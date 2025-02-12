fix normalization for separate convolutions

src/plot.py
@@ -69,16 +69,10 @@ def plot_conv_sep(axs: Axes, sim: Sim, colors: list[str], inst_broadening: float
     Plots convolved data for each vibrational band separately.
     """
 
-    # FIXME: 25/02/12 - Getting the "max intensity" this way is actually wrong. The issue arises
-    #        when two or more vibrational bands overlap. When this overlap occurs, the bands are
-    #        convolved together and can add in intensity, making the max intensity when all bands
-    #        are convolved greater than the max intensity of any given band separately. As such, the
-    #        bands will no longer be normalized properly.
-
     # Need to convolve all bands separately, get their maximum intensities, store the largest, and
-    # then divide all bands by that maximum.
-
-    max_intensity: float = sim.all_conv_data(inst_broadening)[1].max()
+    # then divide all bands by that maximum. If the max intensity was found for all bands convolved
+    # together, it would be inaccurate because of vibrational band overlap.
+    max_intensity: float = max(band.intensities_conv(inst_broadening).max() for band in sim.bands)
 
     for idx, band in enumerate(sim.bands):
         wavelengths_conv: np.ndarray = utils.wavenum_to_wavelen(
addba79a0d97ce7366db3e08aba98f11d4bcac6e
nategphillips <30729007+nategphillips@users.noreply.github.com>
2025-03-10 00:32:14 -0500

    fix colormap fetching

--- src/colors.py
+++ src/colors.py
@@ -2,6 +2,7 @@
 """Provides a list of colors based on the number of bands to be plotted."""
 
 import matplotlib.pyplot as plt
+from cycler import cycler
 from matplotlib.colors import Colormap, to_hex
 
 
@@ -14,7 +15,12 @@ def get_colors(bands: list[tuple[int, int]]) -> list[str]:
     if num_bands < len(colors_small):
         return colors_small
 
-    palette: list[tuple] = plt.cycler("color", plt.cm.tab20c.colors).by_key()["color"]
+    # NOTE: 25/03/10 - The `ListedColormap` class has a `colors` attribute, but for some reason, the
+    #       `get_cmap` method only ever returns a `Colormap`, even when the map is discrete. All
+    #       that to say, ignore this Pyright error. Also, see the link below for more info.
+    #
+    # https://matplotlib.org/stable/gallery/color/individual_colors_from_cmap.html#extracting-colors-from-a-discrete-colormap
+    palette: list[tuple] = cycler("color", plt.get_cmap("tab20c").colors).by_key()["color"]
     colors_medium: list[str] = [to_hex(color) for color in palette]
 
     if num_bands < len(colors_medium):

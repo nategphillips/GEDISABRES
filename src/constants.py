c1fc82a33b30fdbb189a109c1f57c4730f83bb95
nategphillips <30729007+nategphillips@users.noreply.github.com>
2025-04-10 16:23:16 -0500

    important: preparing to merge temp into dev, removing all files from dev first before merging to avoid conflicts

--- src/constants.py
+++
@@ -1,9 +0,0 @@
-# module constants
-"""
-Contains relevant physical constants.
-"""
-
-BOLTZ: float = 1.380649e-23   # Boltzmann constant [J/K]
-PLANC: float = 6.62607015e-34 # Planck constant    [J*s]
-LIGHT: float = 2.99792458e10  # Speed of light     [cm/s]
-AVOGD: float = 6.02214076e23  # Avodagro constant  [1/mol]

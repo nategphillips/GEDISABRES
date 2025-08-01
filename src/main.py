feat: implement correct intensity scaling for all simulations plot

src/main.py
@@ -20,6 +20,7 @@ import sys
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+import numpy as np
 import polars as pl
 import pyqtgraph as pg
 import qdarktheme
@@ -1041,7 +1042,7 @@ class AllSimulationsTab(QWidget):
 
         self.plot_widget.clear()
 
-        custom_tabs = []
+        custom_tabs: list[CustomTab] = []
         for i in range(self.parent_tab_widget.count()):
             tab = self.parent_tab_widget.widget(i)
             if isinstance(tab, CustomTab):
@@ -1065,6 +1066,8 @@ class AllSimulationsTab(QWidget):
         plot_type = self.plot_type_combo.currentText()
         plot_function = map_functions.get(plot_type)
 
+        # TODO: 25/08/01 - Broadening parameters should be pulled from each individual tab instead.
+
         fwhm_selections = {
             "instrument": self.checkbox_instrument.isChecked(),
             "doppler": self.checkbox_doppler.isChecked(),
@@ -1073,12 +1076,68 @@ class AllSimulationsTab(QWidget):
             "predissociation": self.checkbox_predissociation.isChecked(),
         }
 
+        def max_intensity_line():
+            intensities_line: NDArray[np.float64] = np.array([])
+
+            for tab in custom_tabs:
+                _, ins = tab.sim.all_line_data()
+                intensities_line = np.concatenate((intensities_line, ins))
+
+            return intensities_line.max()
+
+        def max_intensity_conv_sep(inst_broadening_wl, granularity):
+            convolved_data: list[NDArray[np.float64]] = []
+            max_intensity: float = 0.0
+
+            for tab in custom_tabs:
+                for band in tab.sim.bands:
+                    intensities_conv = band.intensities_conv(
+                        fwhm_selections,
+                        inst_broadening_wl,
+                        band.wavenumbers_conv(inst_broadening_wl, granularity),
+                    )
+
+                    convolved_data.append(intensities_conv)
+
+                    max_intensity = max(max_intensity, intensities_conv.max())
+
+            return max_intensity
+
+        def max_intensity_conv_all():
+            intensities_conv: NDArray[np.float64] = np.array([])
+
+            for tab in custom_tabs:
+                _, ins = tab.sim.all_conv_data(
+                    fwhm_selections,
+                    self.inst_broadening_spinbox.value(),
+                    self.resolution_spinbox.value(),
+                )
+                intensities_conv = np.concatenate((intensities_conv, ins))
+
+            return intensities_conv.max()
+
+        # TODO: 25/08/01 - Convolving all bands together needs to create a new wavenumber axis
+        #       common to all simulations and then add the contributions of all simulations to the
+        #       plot.
+
         for i, tab in enumerate(custom_tabs):
             bands = tab.get_current_bands()
             colors = get_colors(bands)
 
             if plot_function is not None:
-                if plot_function.__name__ in ("plot_conv_sep", "plot_conv_all"):
+                if plot_function.__name__ == "plot_conv_sep":
+                    broadening = self.inst_broadening_spinbox.value()
+                    resolution = self.resolution_spinbox.value()
+                    plot_function(
+                        self.plot_widget,
+                        tab.sim,
+                        colors,
+                        fwhm_selections,
+                        broadening,
+                        resolution,
+                        max_intensity_conv_sep(broadening, resolution),
+                    )
+                elif plot_function.__name__ == "plot_conv_all":
                     plot_function(
                         self.plot_widget,
                         tab.sim,
@@ -1086,9 +1145,10 @@ class AllSimulationsTab(QWidget):
                         fwhm_selections,
                         self.inst_broadening_spinbox.value(),
                         self.resolution_spinbox.value(),
+                        max_intensity_conv_all(),
                     )
                 else:
-                    plot_function(self.plot_widget, tab.sim, colors)
+                    plot_function(self.plot_widget, tab.sim, colors, max_intensity_line())
 
         self.plot_widget.autoRange()
 

src/plot.py
@@ -55,15 +55,22 @@ def plot_sample(
     )
 
 
-def plot_line(plot_widget: pg.PlotWidget, sim: Sim, colors: list[str]) -> None:
+def plot_line(
+    plot_widget: pg.PlotWidget, sim: Sim, colors: list[str], max_intensity: float | None = None
+) -> None:
     """Plot each rotational line.
 
     Args:
         plot_widget (pg.PlotWidget): A `GraphicsView` widget with a single `PlotItem` inside.
         sim (Sim): The parent simulation.
         colors (list[str]): A list of colors for plotting.
+        max_intensity (float | None, optional): Provided only if multiple simulations are being run
+            together. Defaults to None.
     """
-    max_intensity: float = sim.all_line_data()[1].max()
+    if max_intensity is None:
+        max_intensity = sim.all_line_data()[1].max()
+
+    assert max_intensity is not None
 
     for idx, band in enumerate(sim.bands):
         wavelengths_line: NDArray[np.float64] = utils.wavenum_to_wavelen(band.wavenumbers_line())
@@ -88,18 +95,25 @@ def plot_line(plot_widget: pg.PlotWidget, sim: Sim, colors: list[str]) -> None:
         )
 
 
-def plot_line_info(plot_widget: pg.PlotWidget, sim: Sim, colors: list[str]) -> None:
+def plot_line_info(
+    plot_widget: pg.PlotWidget, sim: Sim, colors: list[str], max_intensity: float | None = None
+) -> None:
     """Plot information about each rotational line.
 
     Args:
         plot_widget (pg.PlotWidget): A `GraphicsView` widget with a single `PlotItem` inside.
         sim (Sim): The parent simulation.
         colors (list[str]): A list of colors for plotting.
+        max_intensity (float | None, optional): Provided only if multiple simulations are being run
+            together. Defaults to None.
     """
-    # In order to show text, a plot must first exist.
-    plot_line(plot_widget, sim, colors)
+    if max_intensity is None:
+        max_intensity = sim.all_line_data()[1].max()
 
-    max_intensity: float = sim.all_line_data()[1].max()
+    assert max_intensity is not None
+
+    # In order to show text, a plot must first exist.
+    plot_line(plot_widget, sim, colors, max_intensity)
 
     for band in sim.bands:
         # Only select non-satellite lines to reduce the amount of data on screen.
@@ -124,6 +138,7 @@ def plot_conv_sep(
     fwhm_selections: dict[str, bool],
     inst_broadening_wl: float,
     granularity: int,
+    max_intensity: float | None = None,
 ) -> None:
     """Plot convolved data for each vibrational band separately.
 
@@ -134,14 +149,16 @@ def plot_conv_sep(
         fwhm_selections (dict[str, bool]): The types of broadening to be simulated.
         inst_broadening_wl (float): Instrument broadening FWHM in [nm].
         granularity (int): Number of points on the wavenumber axis.
+        max_intensity (float | None, optional): Provided only if multiple simulations are being run
+            together. Defaults to None.
     """
     convolved_data: list[tuple[NDArray[np.float64], NDArray[np.float64]]] = []
-    max_intensity: float = 0.0
+    calculated_max_intensity: float = 0.0
 
     # Need to convolve all bands separately, get their maximum intensities, store the largest, and
     # then divide all bands by that maximum. If the max intensity was found for all bands convolved
     # together, it would be inaccurate because of vibrational band overlap.
-    for idx, band in enumerate(sim.bands):
+    for band in sim.bands:
         wavenumbers_conv = band.wavenumbers_conv(inst_broadening_wl, granularity)
         intensities_conv = band.intensities_conv(
             fwhm_selections,
@@ -149,15 +166,17 @@ def plot_conv_sep(
             wavenumbers_conv,
         )
         wavelengths_conv = utils.wavenum_to_wavelen(wavenumbers_conv)
-
         convolved_data.append((wavelengths_conv, intensities_conv))
 
-        max_intensity = max(max_intensity, intensities_conv.max())
+        if max_intensity is None:
+            calculated_max_intensity = max(calculated_max_intensity, intensities_conv.max())
+
+    normalization_factor = max_intensity if max_intensity is not None else calculated_max_intensity
 
     for idx, (wavelengths_conv, intensities_conv) in enumerate(convolved_data):
         plot_widget.plot(
             wavelengths_conv,
-            intensities_conv / max_intensity,
+            intensities_conv / normalization_factor,
             pen=pg.mkPen(colors[idx], width=PEN_WIDTH),
             name=f"{sim.molecule.name} {sim.bands[idx].v_qn_up, sim.bands[idx].v_qn_lo} conv",
         )
@@ -170,6 +189,7 @@ def plot_conv_all(
     fwhm_selections: dict[str, bool],
     inst_broadening_wl: float,
     granularity: int,
+    max_intensity: float | None = None,
 ) -> None:
     """Plot convolved data for all vibrational bands simultaneously.
 
@@ -180,15 +200,22 @@ def plot_conv_all(
         fwhm_selections (dict[str, bool]): The types of broadening to be simulated.
         inst_broadening_wl (float): Instrument broadening FWHM in [nm].
         granularity (int): Number of points on the wavenumber axis.
+        max_intensity (float | None, optional): Provided only if multiple simulations are being run
+            together. Defaults to None.
     """
     wavenumbers_conv, intensities_conv = sim.all_conv_data(
         fwhm_selections, inst_broadening_wl, granularity
     )
     wavelengths_conv: NDArray[np.float64] = utils.wavenum_to_wavelen(wavenumbers_conv)
 
+    if max_intensity is None:
+        max_intensity = intensities_conv.max()
+
+    assert max_intensity is not None
+
     plot_widget.plot(
         wavelengths_conv,
-        intensities_conv / intensities_conv.max(),
+        intensities_conv / max_intensity,
         pen=pg.mkPen(colors[0], width=PEN_WIDTH),
         name=f"{sim.molecule.name} conv all",
     )